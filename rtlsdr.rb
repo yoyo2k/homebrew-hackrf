c172e2445fef8f11adb7f4e7e6b32abe28ff1cf2
Matt Borgerson <mborgerson@gmail.com>
2014-08-12 22:46:54 -0700

    Prevent a shallow copy of the RTL-SDR repository. Apparently their server doesn't like shallow copies?

--- rtlsdr.rb
+++ rtlsdr.rb
@@ -2,7 +2,8 @@ require 'formula'
 
 class Rtlsdr < Formula
   homepage 'http://sdr.osmocom.org/trac/wiki/rtl-sdr'
-  head 'git://git.osmocom.org/rtl-sdr.git'
+  head 'git://git.osmocom.org/rtl-sdr.git',
+    :shallow => false
 
   depends_on 'pkg-config' => :build
   depends_on 'automake' => :build
