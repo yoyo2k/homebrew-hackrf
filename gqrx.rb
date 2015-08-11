0cbeaaa7a08caa1b94268805f448a9e7f00ef21b
Adrian Vasile <yoyo@opennet.ro>
2015-08-11 15:23:17 +0300

    Update gqrx.rb

--- gqrx.rb
+++ gqrx.rb
@@ -27,3 +27,24 @@ class Gqrx < Formula
     bin.install 'gqrx.app/Contents/MacOS/gqrx'
   end
 end
+
+__END__
+
+diff --git a/gqrx.pro b/gqrx.pro
+index 2571518..5e9c600 100644
+--- a/gqrx.pro
++++ b/gqrx.pro
+@@ -233,7 +233,12 @@
+ }
+
+ macx {
+-    LIBS += -lboost_system-mt -lboost_program_options-mt
++    LIBS += -lboost_system-mt -lboost_program_options-mt -lgnuradio-audio -lgnuradio-core -lgnuradio-osmosdr
++    LIBPATH += /usr/local/lib
++    INCLUDEPATH += /usr/local/include
++    INCLUDEPATH += /usr/local/include/gnuradio
++    INCLUDEPATH += /usr/local/include/osmosdr
++    INCLUDEPATH += /opt/local/include
+ }
+
+ OTHER_FILES += \
