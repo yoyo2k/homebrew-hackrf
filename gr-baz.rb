9c15409fe16eca3c1eed14d0d02c9573ba30bbce
Adrian Vasile <yoyo@opennet.ro>
2015-08-11 13:12:38 +0300

    Update gr-baz.rb

--- gr-baz.rb
+++ gr-baz.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class GrBaz < Formula
   homepage 'http://wiki.spench.net/wiki/Gr-baz'
-  head 'https://github.com/balint256/gr-baz.git', :branch => "3.6"
+  head 'https://github.com/balint256/gr-baz.git', :branch => "master"
 
   depends_on 'cmake' => :build
   depends_on 'gnuradio'
